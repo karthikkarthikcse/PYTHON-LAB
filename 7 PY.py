9e98fa7aa3c1d50ca308a44ce8eb38fa3307d012
Karthik.T.R <trrameshbabutrrameshbabu@gmail.com>
2025-09-15 15:33:08 +0530

    Add files via upload

---
+++ 7 PY.py
@@ -0,0 +1,53 @@
+class Node:
+ def __init__(self,data):
+     self.data=data
+     self.left=None
+     self.right=None
+class BinaryTree:
+     def __init__(self):
+         self.root=None
+     def insert(self,data):
+         new_node=Node(data)
+ if self.root is None:
+     self.root=new_node
+     return
+     queue=[self.root]
+     while queue:
+     current=queue.pop(0)
+ if current.left is None:
+     current.left=new_node
+ break
+ else:
+     queue.append(current.left)
+ if current.right is None:
+     current.right=new_node
+     break
+ else:
+     queue.append(current.right)
+ def inorder(self,node):
+ if node:
+     self.inorder(node.left)
+     print(node.data,end=" ")
+     self.inorder(node.right)
+     def preorder(self,node):
+         if node:
+             print(node.data,end=" ")
+             self.preorder(node.left)
+             self.preorder(node.right)
+ def postorder(self,node):
+     if node:
+        Q self.postorder(node.left)
+         self.postorder(node.right)
+         print(node.data,end=" ")
+if __name__=="__main__":
+         bt=BinaryTree()
+         n=int(input("enter the number of nodes to be in the binary tree:"))
+         for i in range(n):
+         val=int(input(f"enter the value of the node {i+1}:"))
+         bt.insert(val)
+         print("\ninorder Binarytree:",end=" ")
+         bt.inorder(bt.root)
+         print("\npostorder BinaryTree:",end=" ")
+         bt.postorder(bt.root)
+         print("\npreorder BinaryTree:",end=" ")
+         bt.preorder(bt.root)
